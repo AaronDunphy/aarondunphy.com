2a73ce72cab464d956369ed6934e50799041bc50
Aaron <aaronjohndunphy@gmail.com>
2020-08-08 14:01:30 +0100

    Basic website structure in place

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Footer = () => (
+    <footer style={{
+        margin: `40px 0`,
+        fontSize: 14
+    }}>
+        <a href="https://twitter.com/aarondunphy" target="_blank">Twitter</a>
+        <span style={{ padding: `0 5px` }}>•</span>
+        <a href="https://instagram.com/aarondunphy" target="_blank">Instagram</a>
+        <span style={{ padding: `0 5px` }}>•</span>
+        <a href="https://linkedin.com//in/aarondunphy" target="_blank">LinkedIn</a>
+    </footer>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,58 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = (data) => {
+
+    const { nodes } = data.data.allMarkdownRemark
+    console.log('nodes', nodes)
+    const posts = nodes.map((node, index) => {
+        return (
+            <div key={index}>
+                <h3 style={{margin: 0}}>
+                    <Link
+                        to={ node.frontmatter.slug }
+                        style={{
+                            color: "black",
+                        }}
+                    >
+                        { node.frontmatter.title }
+                    </Link>
+                </h3>
+                <p>{ node.frontmatter.description }</p>
+
+            </div>
+        )
+    })
+    return (
+        <Layout>
+            <SEO title="Home" />
+            <p style={{fontSize: 40, fontWeight: 700, marginBottom: 20}}>Hey, I'm Aaron 👋</p>
+            <h1 style={{
+                margin: `0 0 5px 0`,
+                fontSize: 24
+            }}>
+                Full Stack Developer based in Leigh-on-Sea, UK.
+            </h1>
+            <p>Laravel &amp; JavaScript. Powered by ☕️</p>
+            { posts }
+        </Layout>
+    )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+    query {
+        allMarkdownRemark {
+            nodes {
+                frontmatter {
+                    title
+                    description
+                    slug
+                }
+            }
+        }
+    }
+`
