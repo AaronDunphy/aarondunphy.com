de3661db472eaca963fdf37015962aa72d310346
Aaron <aaronjohndunphy@gmail.com>
2020-08-17 18:12:02 +0100

    Top 5 tech podcasts added

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Header from "../components/header"
+import { Link } from "gatsby"
 
 const AboutPage = () => (
     <Layout>
@@ -10,7 +11,7 @@ const AboutPage = () => (
         <Header />
         
         <h1>About</h1>
-        <p>I'm a full stack developer based in <a href="https://en.wikipedia.org/wiki/Leigh-on-Sea" target="_blank">Leigh-on-Sea</a>, UK. I have a passion for coding and get most enjoyment from solving problems with writing elegant/maintainable code and sharing knowledge. I love learning so I'm always keeping up to date by watching conference talks, listening to podcasts and reading blog posts.</p>
+        <p>I'm a full stack developer based in <a href="https://en.wikipedia.org/wiki/Leigh-on-Sea" target="_blank">Leigh-on-Sea</a>, UK. I have a passion for coding and get most enjoyment from solving problems with writing elegant/maintainable code and sharing knowledge. I love learning so I'm always keeping up to date by watching conference talks, listening to <Link to="/blog/my-top-5-tech-podcasts-2020">podcasts</Link> and reading blog posts.</p>
         
         <p>My coding journey started unexpectedly in my first year of University in 2012. I went to uni for graphic design but in our first year we had to build a basic website and I thought writing code was so cool. I was hooked so I switched my degree to Web Technologies. I've been working in the industry since graduating in 2015 with a first class honours.</p>
 
